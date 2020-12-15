3a6208bcfdaaae3b932c04497dfa4836cdb30e37
qianjing <740363495@qq.com>
2020-12-15 10:42:17 +0800

    路由修改

--- Modules/Admin/Routes/api.php
+++ Modules/Admin/Routes/api.php
@@ -31,6 +31,5 @@ Route::group([
         Route::get('admin/getMenuListAjax', 'AdminController@getMenuListAjax'); //获取菜单列表ajax
         Route::post('admin/changeMenuStateAjax/{id}/{status}', 'AdminController@changeMenuStateAjax'); //修改菜单状态ajax
         Route::get('admin/getMenuDirAjax', 'AdminController@getMenuDirAjax'); //获取菜单目录ajax
-
     }
 );
