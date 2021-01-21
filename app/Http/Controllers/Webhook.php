60edfa2008dd56665fc42781ba2f49e0996ca182
qianjing <740363495@qq.com>
2021-01-21 15:56:26 +0800

    Webhook

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -20,8 +20,12 @@ class Webhook extends BaseController
 
     public function index()
     {
+        $json_content = file_get_contents('php://input');
+        $data = json_decode($json_content, true);
+
         //github发送过来的签名
-        return $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+        $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+        echo $signature;
         if (!$signature) {
             return http_response_code(404);
         }
@@ -29,7 +33,7 @@ class Webhook extends BaseController
         list($algo, $hash) = explode('=', $signature, 2);
 
         //计算签名
-        $payloadHash = hash_hmac($algo, $json, $this->valid_token);
+        $payloadHash = hash_hmac($algo, $json_content, $this->valid_token);
         if ($hash !== $payloadHash){
             return http_response_code(404);
         }
@@ -37,22 +41,16 @@ class Webhook extends BaseController
         //调用接口被允许的ip地址
         $client_ip = $_SERVER['REMOTE_ADDR'];
 
-        $fs = fopen('/webhook/auto_hook.log', 'a');
+        $fs = fopen('./auto_hook.log', 'a');
         fwrite($fs, 'Request on ['.date("Y-m-d H:i:s").'] from ['.$client_ip.']'.PHP_EOL);
-
-        $json_content = file_get_contents('php://input');
-        $data = json_decode($json_content, true);
 
         fwrite($fs, 'Data: '.json_encode($data).PHP_EOL);
         fwrite($fs, '======================================================================='.PHP_EOL);
         $fs and fclose($fs);
 
-        if (empty($data['token']) || $data['token'] !== $valid_token) {
-            exit('aInvalid token request');
-        }
-
         $cmd = "cd ".$this->web_path." && git checkout  master && git pull origin master";
-        return shell_exec($cmd);
+        echo shell_exec($cmd);
+
 
     }
 }
