36ca7de7c8e060fac8dd1689d49ee4c7a34c827b
qianjing <740363495@qq.com>
2021-01-26 15:35:15 +0800

    微信用户添加测试

--- Modules/Admin/Http/Controllers/UserController.php
+++ Modules/Admin/Http/Controllers/UserController.php
@@ -17,8 +17,23 @@ class UserController extends AdminBaseController
 
     public function userList()
     {
-        $user = $this->faceUserModel::whereOpenid('oLfzT6HKHJagFJm5GrS7w2WxEXFQ')->first();
+        $userData = array(
+            'openid' => 'oLfzT6HKHJagFJm5GrS7w2WxEXFQ',
+            'nickname' => '低调的小香菇😈',
+            'avatar' => 'http://thirdwx.qlogo.cn/mmopen/fU6cXgFxgovYcP0sHjfc9ZoxXXfhCxkx8leZiccelWQSibuSCPMicJKmRg7T0EchQYttL7fxUa3ibebwfLNs2WquyHWcMM41fVDR/132',
+            'sex' => 1,
+            'city' => '',
+            'province' => '',
+            'country' => '冰岛',
+            'is_subscribe' => 1,
+            'user_state' => 1,
+            'subscribe_time' => 1611646415,
+        );
 
-        dump($user);
+//        $user = $this->faceUserModel::whereOpenid('oLfzT6HKHJagFJm5GrS7w2WxEXFQ')->first();
+
+        $res = $this->faceUserModel::create($userData);
+
+        dump($res);
     }
 }
